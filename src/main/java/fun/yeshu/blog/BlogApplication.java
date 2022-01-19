1ebe3779f8230e103841c2149d6f984bfb0817d5
yeshu <guan.yeshu@gmail.com>
2022-01-19 15:19:09 +0800

    write a code to insert metadata in tables

--- src/main/java/fun/yeshu/blog/BlogApplication.java
+++ src/main/java/fun/yeshu/blog/BlogApplication.java
@@ -1,12 +1,17 @@
 package fun.yeshu.blog;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import fun.yeshu.blog.entity.Role;
+import fun.yeshu.blog.repository.RoleRepository;
+
 @SpringBootApplication
-public class BlogApplication {
+public class BlogApplication implements CommandLineRunner{
 
 	@Bean
 	public ModelMapper modelMapper() {
@@ -15,6 +20,21 @@ public class BlogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
+	}
+
+	@Autowired
+	private RoleRepository roleRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Role adminRole = new Role();
+		adminRole.setName("ROLE_ADMIN");
+		roleRepository.save(adminRole);
+
+		Role userRole = new Role();
+		userRole.setName("ROLE_USER");
+		roleRepository.save(userRole);
+		
 	}
 
 }
